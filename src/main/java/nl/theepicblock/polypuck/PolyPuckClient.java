0d6c024439642d63463fe439186767338ad53a04
TheEpicBlock <git.teb@theepicblock.nl>
2021-02-08 22:07:33 +0100

    Remove dependency on PolyMc, as you're not supposed to have PolyMc on the client. Move serverside code to PolyPuckServer to prevent crashes as PolyMc isn't available on a client

--- src/main/java/nl/theepicblock/polypuck/PolyPuckClient.java
+++ src/main/java/nl/theepicblock/polypuck/PolyPuckClient.java
@@ -2,12 +2,15 @@ package nl.theepicblock.polypuck;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.fabricmc.fabric.impl.screenhandler.client.ClientNetworking;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class PolyPuckClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		ClientPlayNetworking.registerGlobalReceiver(PolyPuck.CHANNEL_ID, (client,handler,packet,sender) -> {
+		if (FabricLoader.getInstance().isModLoaded("polymc")) {
+			System.out.println("[PolyPuck] warning: PolyMc should not be installed on the client");
+		}
+		ClientPlayNetworking.registerGlobalReceiver(PolyPuckServer.CHANNEL_ID, (client, handler, packet, sender) -> {
 			//We're not handling the packet, only registering the channel so the server can pick it up.
 		});
 	}
--- src/main/java/nl/theepicblock/polypuck/PolyPuckServer.java
+++ src/main/java/nl/theepicblock/polypuck/PolyPuckServer.java
@@ -3,16 +3,21 @@ package nl.theepicblock.polypuck;
 import io.github.theepicblock.polymc.api.PolyMap;
 import io.github.theepicblock.polymc.api.misc.PolyMapProvider;
 import io.github.theepicblock.polymc.impl.NOPPolyMap;
+import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
 
-public class PolyPuck implements ModInitializer {
+public class PolyPuckServer implements DedicatedServerModInitializer {
 	private final static PolyMap NOPMAP = new NOPPolyMap();
 	public final static Identifier CHANNEL_ID = new Identifier("polypuck","enabled");
 
 	@Override
-	public void onInitialize() {
+	public void onInitializeServer() {
+		if (!FabricLoader.getInstance().isModLoaded("polymc")) {
+			throw new IllegalStateException("PolyPuck must be used together with PolyMc on the server");
+		}
 		PolyMapProvider.EVENT.register(player -> ServerPlayNetworking.canSend(player, CHANNEL_ID) ? NOPMAP : null);
 	}
 }
