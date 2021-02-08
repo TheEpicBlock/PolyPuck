75b63688e6a3018806a249dfa53fba6890b3a232
TheEpicBlock <git.teb@theepicblock.nl>
2021-02-08 20:09:46 +0100

    Init project

---
+++ src/main/java/nl/theepicblock/polypuck/PolyPuck.java
@@ -0,0 +1,10 @@
+package nl.theepicblock.polypuck;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PolyPuck implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		
+	}
+}
